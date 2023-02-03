0eff95e6a9b534abdebf4b2c227ee38b253f8fe7
leeclaire156 <lee.claire156@gmail.com>
2023-02-02 22:04:44 -0500

    Adds server boilerplate code

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-//TODO: connect to database with mysql2
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+const cTable = require('console.table');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: '',
+        database: 'courses_db'
+    },
+    console.log(`Connected to the courses_db database.`)
+);
+
+//TODO: create Routes
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
