33a111b72f379909f3dea32cfeb8a4f4362f2416
leeclaire156 <lee.claire156@gmail.com>
2023-02-04 01:31:32 -0500

    Creates get path to show department as table in console log

--- server.js
+++ server.js
@@ -17,13 +17,35 @@ const db = mysql.createConnection(
         // MySQL username,
         user: 'root',
         // MySQL password
-        password: '',
-        database: 'courses_db'
+        password: '^^@*M0n13$',
+        database: 'company_db'
     },
     console.log(`Connected to the courses_db database.`)
 );
 
-//TODO: create Routes
+//TODO: create API Routes
+//Used to add new information (Add options)
+// app.post('./');
+
+//Used to read information (View options)
+app.get('/api/employee', (req, res) => {
+    const sql = `SELECT id as Department_ID, name as Department_Name FROM department`;
+
+    db.query(sql, (err, data) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        const dataObject = (res.json({
+            data: data
+        }));
+        const table = cTable.getTable(data)
+        console.log(table);
+    });
+});
+
+//Used to update information (Update options)
+// app.put();
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
